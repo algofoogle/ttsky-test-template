f3d84a6202d2129e148f646d529d62cc6941908f
Anton Maurovic <anton@foogle.com>
2026-05-08 21:06:12 +0930

    Q5.9 phase acc. G1=59.94Hz. Basic music.

--- src/project.v
+++ src/project.v
@@ -69,31 +69,47 @@ module tt_um_algofoogle_test (
             a <= a + 1;
 
     localparam RSP = 6; // Rate scaling point: For B above this, phase is multiplied. Below it, phase is divided. This maintains desired frequency.
-    localparam B = 12; // Bit depth of audio samples. 8 smooth, minor harmonics. 9 smooth, distant harmonics. 10 ~clean. 6 present harmonics. 5 workable. 4 barely passes. 3 Atari.
+    localparam B = 5; // Bit depth of audio samples. 8 smooth, minor harmonics. 9 smooth, distant harmonics. 10 ~clean. 6 present harmonics. 5 workable. 4 barely passes. 3 Atari.
     localparam BS = B-RSP; // Bit left shift factor.
     localparam SB = RSP-B; // Bit right shift factor.
-    localparam SUB = 8; // Sub-resolution of the voice phase accumulator. SUB+B+1 is the total phase bit depth.
+    localparam SUB = 9; // Sub-resolution of the voice phase accumulator. SUB+B+1 is the total phase bit depth: Larger gives more tonal precision. 4+8 is minimum.
 
-    // Tuning based on B1=59.94Hz (making it possible for us to tune based on VSYNC):
-    // C2 = B1*2^(1/12) ~= 59.94*1.0595 ~= 63.5Hz
-    // Sampling rate (based on line_end) is (25175000/800)=31468.75Hz
-    // Hence, each sample is a fractional slice:
-    // n = 63.5/31468.75 ~= 0.0020179
-    // This becomes a portion of the full phase ramp range of 2^(B+SUB)=8192
-    // Hence, the frequency factor is C2=0.0020179*8192=16.53 => round up to 17
-    //                       f*1E6           RampRange      Fs*1000 Round /1000
-    localparam [63:0] P_C  = (( 63_504_217 * (2**(B+SUB+1)) / 31468750)+500)/1000;
-    localparam [63:0] P_Cs = (( 67_280_375 * (2**(B+SUB+1)) / 31468750)+500)/1000;
-    localparam [63:0] P_D  = (( 71_281_074 * (2**(B+SUB+1)) / 31468750)+500)/1000;
-    localparam [63:0] P_Ds = (( 75_519_667 * (2**(B+SUB+1)) / 31468750)+500)/1000;
-    localparam [63:0] P_E  = (( 80_010_300 * (2**(B+SUB+1)) / 31468750)+500)/1000;
-    localparam [63:0] P_F  = (( 84_767_960 * (2**(B+SUB+1)) / 31468750)+500)/1000;
-    localparam [63:0] P_Fs = (( 89_808_526 * (2**(B+SUB+1)) / 31468750)+500)/1000;
-    localparam [63:0] P_G  = (( 95_148_819 * (2**(B+SUB+1)) / 31468750)+500)/1000;
-    localparam [63:0] P_Gs = ((100_806_662 * (2**(B+SUB+1)) / 31468750)+500)/1000;
-    localparam [63:0] P_A  = ((106_800_938 * (2**(B+SUB+1)) / 31468750)+500)/1000;
-    localparam [63:0] P_As = ((113_151_652 * (2**(B+SUB+1)) / 31468750)+500)/1000;
-    localparam [63:0] P_B  = ((119_880_000 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    // // Tuning based on B1=59.94Hz (making it possible for us to tune based on VSYNC):
+    // // C2 = B1*2^(1/12) ~= 59.94*1.0595 ~= 63.5Hz
+    // // Sampling rate (based on line_end) is (25175000/800)=31468.75Hz
+    // // Hence, each sample is a fractional slice:
+    // // n = 63.5/31468.75 ~= 0.0020179
+    // // This becomes a portion of the full phase ramp range of 2^(B+SUB)=8192
+    // // Hence, the frequency factor is C2=0.0020179*8192=16.53 => round up to 17
+    // //                       f*1E6           RampRange      Fs*1000 Round /1000
+    // // Maybe make 60Hz G1? That will give everything slightly better tuning as the base.
+    // localparam [63:0] P_C  = (( 63_504_217 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    // localparam [63:0] P_Cs = (( 67_280_375 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    // localparam [63:0] P_D  = (( 71_281_074 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    // localparam [63:0] P_Ds = (( 75_519_667 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    // localparam [63:0] P_E  = (( 80_010_300 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    // localparam [63:0] P_F  = (( 84_767_960 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    // localparam [63:0] P_Fs = (( 89_808_526 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    // localparam [63:0] P_G  = (( 95_148_819 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    // localparam [63:0] P_Gs = ((100_806_662 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    // localparam [63:0] P_A  = ((106_800_938 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    // localparam [63:0] P_As = ((113_151_652 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    // localparam [63:0] P_B  = ((119_880_000 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    // ×2^(1÷12)
+    // Tuning based on G1=59.94Hz:
+    // C2 = G1*2^(5/12) ~= 80.0103Hz
+    localparam [63:0] P_C  = (( 80_010_301 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    localparam [63:0] P_Cs = (( 84_767_961 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    localparam [63:0] P_D  = (( 89_808_526 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    localparam [63:0] P_Ds = (( 95_148_819 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    localparam [63:0] P_E  = ((100_806_662 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    localparam [63:0] P_F  = ((106_800_938 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    localparam [63:0] P_Fs = ((113_151_653 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    localparam [63:0] P_G  = ((119_880_000 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    localparam [63:0] P_Gs = ((127_008_436 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    localparam [63:0] P_A  = ((134_560_750 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    localparam [63:0] P_As = ((142_562_149 * (2**(B+SUB+1)) / 31468750)+500)/1000;
+    localparam [63:0] P_B  = ((151_039_335 * (2**(B+SUB+1)) / 31468750)+500)/1000;
 
     initial $display("AAANTON", P_C, P_Cs, P_D, P_Ds, P_E, P_F, P_Fs, P_G, P_Gs, P_A, P_As, P_B);
 
@@ -102,37 +118,45 @@ module tt_um_algofoogle_test (
     // // Phase comes from vertical line index (has 60Hz hard sync): //NOTE: hvsync_generator is currently hacked to use 512-line frame length.
     // wire [B:0] p = (B>=RSP) ? v[B:0]<<BS : v[B+SB:SB];
     // Phase comes from 'voice' phase accumulator:
-    reg [B+SUB+1:0] pi;
+    reg [B+SUB:0] pi;
     always @(*) begin
-        pi = P_B;//<<1;
-        case (frame_counter[9:6])
+        pi = P_G<<1;
+        // C   G   C2  G   Gs  F   E   Cs
+        case (frame_counter[6:4])
         4'd0:  pi = P_C;
-        4'd1:  pi = P_D;
-        4'd2:  pi = P_E;
-        4'd3:  pi = P_F;
+        4'd1:  pi = P_E;
+        4'd2:  pi = P_G;
+        4'd3:  pi = P_C<<1;
         4'd4:  pi = P_G;
-        4'd5:  pi = P_A;
-        4'd6:  pi = P_B;
-        4'd7:  pi = P_G;
-        4'd8:  pi = P_Fs;
+        4'd5:  pi = P_F;
+        4'd6:  pi = P_D;
+        4'd7:  pi = P_B;
+        4'd8:  pi = P_C<<1;
         4'd9:  pi = P_D;
-        4'd10: pi = P_Cs;
-        4'd11: pi = P_D;
+        4'd10: pi = P_E;
+        4'd11: pi = P_G;
 
 
+        // 4'd8:  pi = P_C;
+        // 4'd9:  pi = P_D;
+        // 4'd10: pi = P_E;
+        // 4'd11: pi = P_G;
         // 4'd0:  pi = P_C;
-        // 4'd1:  pi = P_Cs;
-        // 4'd2:  pi = P_D;
-        // 4'd3:  pi = P_Ds;
-        // 4'd4:  pi = P_E;
+        // 4'd1:  pi = P_G;
+        // 4'd2:  pi = P_C<<1;
+        // 4'd3:  pi = P_G;
+        // 4'd4:  pi = P_Gs;
         // 4'd5:  pi = P_F;
-        // 4'd6:  pi = P_Fs;
-        // 4'd7:  pi = P_G;
-        // 4'd8:  pi = P_Gs;
-        // 4'd9:  pi = P_A;
-        // 4'd10: pi = P_As;
-        // 4'd11: pi = P_B;
+        // 4'd6:  pi = P_E;
+        // 4'd7:  pi = P_Cs;
+        // 4'd8:  pi = P_C;
+        // 4'd9:  pi = P_D;
+        // 4'd10: pi = P_E;
+        // 4'd11: pi = P_G;
+
+
         endcase
+        pi = pi<<2;
     end
 
     wire [B:0] p;
@@ -147,8 +171,8 @@ module tt_um_algofoogle_test (
         .sample_out(p)
     );
 
-    // Generate a signed square wave from the phase:
-    wire signed [B-1:0] sq_sample = ({B{p[B]}} ^ (1<<(B-1)));
+    // // Generate a signed square wave from the phase:
+    // wire signed [B-1:0] sq_sample = ({B{p[B]}} ^ (1<<(B-1)));
     // Generate a signed triangle wave from the phase:
     wire signed [B-1:0] tr_sample = (({B{p[B]}} ^ p[B-1:0]) + (1<<(B-1))); //NOTE: midpoint bias added for making this signed. Is there a way to avoid that?
 
@@ -172,7 +196,13 @@ module tt_um_algofoogle_test (
     //                         attenuated_sample(tr_sample, exp_atten) +
     //     (frame_counter[7] ? {3'd0,{3{vsync}}} : 0);
     //                         // 0;
-    wire signed [B:0] mixed_sample = frame_counter[10] ? $signed({1'd0,{5{v>240}}}): tr_sample;
+    wire sq60 = v>240;
+    wire signed [B-1:0] sq_sample = 0;// $signed( {{B-1{sq60}},{1{~sq60}}} );
+    wire signed [B:0] mixed_sample =
+        sq_sample +
+        tr_sample;
+    
+    // frame_counter[10] ? $signed({1'd0,{5{v>240}}}): tr_sample;
         // (frame_counter[7] ? attenuated_sample(sq_sample, exp_atten) : 0);
     // Average mixing of the two samples:
     wire signed [B-1:0] sample = mixed_sample[B:1];
